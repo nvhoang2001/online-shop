2120ad46ac45bd3f69da9b876cd0194fea74bb13
nvhoang2001 <ngoviethoang2424@gmail.com>
2021-08-19 11:03:23 +0700

    Add some product route const

--- src/config.js
+++ src/config.js
@@ -3,3 +3,6 @@ export const signInURL = "/sign-in";
 export const baseURL = "/";
 export const driveViewLink = "https://drive.google.com/uc?export=view&id=";
 export const checkoutPage = "/checkout";
+export const TECH_PROD_DIR = "/products/technology";
+export const FASHION_PROD_DIR = "/products/fashion";
+export const PROD_DIR = "/products";
