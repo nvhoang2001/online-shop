88953b1630baff72a2d66c6b16f252e04747e489
nvhoang2001 <ngoviethoang2424@gmail.com>
2021-08-24 10:24:52 +0700

    Add conditionally render register page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import Homepage from "./pages/Homepage/Homepage";
 import RegisterPage from "./pages/Register/RegisterPage";
@@ -6,15 +6,20 @@ import RegisterPage from "./pages/Register/RegisterPage";
 import { signUpURL } from "./config";
 import "./reset-css.scss";
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
+	const isSignIn = !!useSelector((store) => store.user.auth);
+
 	return (
 		<Switch>
 			<Route path="/" exact>
 				<Homepage />
 			</Route>
+
 			<Route path={signUpURL} exact>
-				<RegisterPage />
+				{!isSignIn && <RegisterPage />}
+				{isSignIn && <Redirect to="/" />}
 			</Route>
 		</Switch>
 	);
