ee156ea05a89e5f8701f83c127ae886c1792a16a
nvhoang2001 <ngoviethoang2424@gmail.com>
2021-10-26 22:14:52 +0700

    Expand product list, add navigate buttons

--- src/sections/ProductByCategory/ProductByCategory.jsx
+++ src/sections/ProductByCategory/ProductByCategory.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import ProductList from "../../components/Product/ProductList";
 import { driveViewLink } from "../../config";
@@ -14,6 +14,9 @@ const NONE = "NONE",
 
 const ProductByCategory = (props) => {
 	const [sortType, setSortType] = useState(NONE);
+	const [listClass, setListClass] = useState("");
+	const [activeList, setActiveList] = useState(true);
+	const [activePosition, setActivePosition] = useState(0);
 
 	const { type, driveId } = props;
 	const items = useSelector((store) => store.products.items);
@@ -34,13 +37,16 @@ const ProductByCategory = (props) => {
 			default:
 				break;
 		}
-		prodItems.length = ITEM_LIMIT;
+		prodItems.length = ITEM_LIMIT * 5;
 		return prodItems;
 	};
 
-	const fileredItems = filterItemBy(sortType);
+	const fileredItems = filterItemBy(sortType).slice(
+		activePosition * ITEM_LIMIT,
+		(activePosition + 1) * ITEM_LIMIT,
+	);
 
-	const btnContainerClickHandler = (e) => {
+	const controlBtnContainerClickHandler = (e) => {
 		const clickedEl = e.target;
 		if (!clickedEl.dataset.type) {
 			return;
@@ -60,7 +66,35 @@ const ProductByCategory = (props) => {
 				setSortType(NONE);
 				break;
 		}
+		setActiveList((ac) => !ac);
 	};
+
+	const slideProductLeftHandler = () => {
+		if (activePosition === 0) {
+			return;
+		}
+
+		setActivePosition((pos) => pos - 1);
+		setActiveList((ac) => !ac);
+	};
+	const slideProductRightHandler = () => {
+		if (activePosition === 5) {
+			return;
+		}
+
+		setActivePosition((pos) => pos + 1);
+		setActiveList((ac) => !ac);
+	};
+
+	useEffect(() => {
+		setListClass("product-by-category__list--active");
+		const timer = setTimeout(() => {
+			setListClass("");
+		}, 500);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [activeList]);
 
 	return (
 		<section className="product-by-category">
@@ -70,47 +104,64 @@ const ProductByCategory = (props) => {
 					<img src={`${driveViewLink}${driveId}`} alt="" />
 				</div>
 				<div className="product-by-category__list-container">
-					<div
-						className="product-by-category__btn-container"
-						onClick={btnContainerClickHandler}
-					>
-						<button
-							className={`product-by-category__btn ${
-								sortType === NONE ? "product-by-category__btn--active" : ""
-							}`}
-							data-type={NONE}
+					<div className="product-by-category__btn-container">
+						<div
+							className="product-by-category__control-btns"
+							onClick={controlBtnContainerClickHandler}
 						>
-							ALL
-						</button>
-						<button
-							className={`product-by-category__btn ${
-								sortType === SOLD ? "product-by-category__btn--active" : ""
-							}`}
-							data-type={SOLD}
-						>
-							TOP SOLD
-						</button>
-						<button
-							className={`product-by-category__btn ${
-								sortType === VIEW ? "product-by-category__btn--active" : ""
-							}`}
-							data-type={VIEW}
-						>
-							TOP VIEW
-						</button>
-						<button
-							className={`product-by-category__btn ${
-								sortType === RATING ? "product-by-category__btn--active" : ""
-							}`}
-							data-type={RATING}
-						>
-							TOP RATING
-						</button>
+							<button
+								className={`product-by-category__btn ${
+									sortType === NONE ? "product-by-category__btn--active" : ""
+								}`}
+								data-type={NONE}
+							>
+								ALL
+							</button>
+							<button
+								className={`product-by-category__btn ${
+									sortType === SOLD ? "product-by-category__btn--active" : ""
+								}`}
+								data-type={SOLD}
+							>
+								TOP SOLD
+							</button>
+							<button
+								className={`product-by-category__btn ${
+									sortType === VIEW ? "product-by-category__btn--active" : ""
+								}`}
+								data-type={VIEW}
+							>
+								TOP VIEW
+							</button>
+							<button
+								className={`product-by-category__btn ${
+									sortType === RATING ? "product-by-category__btn--active" : ""
+								}`}
+								data-type={RATING}
+							>
+								TOP RATING
+							</button>
+						</div>
+
+						<div className="product-by-category__navigate-btns">
+							<button
+								className="product-by-category__btn--slide product-by-category__btn--slide-left"
+								onClick={slideProductLeftHandler}
+							>
+								<span id="shape"></span>
+							</button>
+							<button
+								className="product-by-category__btn--slide product-by-category__btn--slide-right"
+								onClick={slideProductRightHandler}
+							>
+								<span id="shape"></span>
+							</button>
+						</div>
 					</div>
 
 					<ProductList
 						products={fileredItems}
-						className="product-by-category__list"
+						className={`product-by-category__list ${listClass}`}
 						baseClass="product-by-category"
 					/>
 				</div>
